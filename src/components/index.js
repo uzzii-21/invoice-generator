053981c90830fe7bbd61090fa4a7206be9fc0c6f
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-03-15 02:29:12 +0500

    removed unuse comp on pkgs

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
-import DatePicker from './DatePicker/DatePicker';
 import Input from './Input/Input';
 import SideBarButton from './SideBarButton/SideBarButton';
 import ItemsList from './ItemsList/ItemsList';
 import Toggle from './Toggle/Toggle';
 
-export { DatePicker, Input, SideBarButton, ItemsList, Toggle };
+export { Input, SideBarButton, ItemsList, Toggle };
